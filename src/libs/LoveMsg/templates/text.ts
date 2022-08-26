62d6fba2e6cdd4f42e90e5d4edfa4f99cd63ee82
zhufengbo1 <110660345+zhufengbo1@users.noreply.github.com>
2022-08-26 10:17:16 +0800

    Update text.ts

--- src/libs/LoveMsg/templates/text.ts
+++ src/libs/LoveMsg/templates/text.ts
@@ -2,29 +2,45 @@
  * @description 纯文本模板-企业微信消息通知
  * https://open.work.weixin.qq.com/api/doc/90000/90135/90236
  */
+ import dayjs, { weekToday } from '../../../utils/dayjs'
 
-import dayjs, { weekToday } from '../../../utils/dayjs'
+
+ 
 
 export const textTemplate = (data: TextTemplateProps) => {
-  const { caiHongpi, sayLove, songLyrics, oneMagazines, netEaseCloud, oneWord, dayEnglish } = data
+  const { caiHongpi, sayLove, songLyrics, oneMagazines, netEaseCloud, oneWord, dayEnglish,weather } = data
 
-  let text = '早安呀，我可爱的鱼崽崽~\n'
+  const week = weekToday()
+  var date=null;
+  let yy = new Date().getFullYear();
+    let mm = new Date().getMonth()+1;
+    let dd = new Date().getDate();
+    let hh = new Date().getHours();
+    let mf = new Date().getMinutes()<10 ? '0'+new Date().getMinutes() : new Date().getMinutes();
+    let ss = new Date().getSeconds()<10 ? '0'+new Date().getSeconds() : new Date().getSeconds();
+    date = yy+'-'+mm+'-'+dd+' '+hh+':'+mf+':'+ss;
+    let text = '丹丹小闹钟为您推送\n'+`今天是${date}`
+   
+ 
 
   // 工作日/休息日，需要排除节假日
-  const week = weekToday()
+
   if (['星期六', '星期日'].includes(week)) {
     text += `
-如果我鱼崽崽已经起床啦！崽崽向你说早安呦~，记得吃早饭呀😆\n
+ 丹丹小闹钟为您推送\n
+
 嗯哼哼~今天可是${week}哦，上班别迟到了哦~`
   }
   else {
     text += `
-如果我鱼崽崽还没起床呀！崽崽就等着鱼崽起床给我说早安呦🤣
-嗯哼~，既然今天是${week}，就让你再睡会懒觉~下次可不能啦~😝\n`
+     早啊，小丹丹，
+今儿个是${week}，
+又是一个辛苦的一天，加油打票人😝\n `
   }
-
+  text+=weather;
   // 添加笑话
   if (caiHongpi) {
+    
     //     text += `
     // 彩虹屁：
     text += `
@@ -63,6 +79,7 @@ ${sayLove.content}\n`
     text += `
 『每日英语（${dayjs(dayEnglish.date).format('ll')}』${dayEnglish.content}`
   }
+
 
   return {
     msgtype: 'text',
